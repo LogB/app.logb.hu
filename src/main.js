0e3658a7e205b9e39c778697f234bef9d9c50b48
remenyo <remenyo@gmail.com>
2019-02-15 23:30:38 +0100

    Resorted imports

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import "roboto-fontface/css/roboto/roboto-fontface.css";
-import "material-design-icons-iconfont/dist/material-design-icons.css";
 import i18n from "./i18n";
 import Vuelidate from "vuelidate";
 import axios from "axios";
